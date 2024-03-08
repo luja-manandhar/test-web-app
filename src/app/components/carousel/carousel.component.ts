f64fba80afc4a412c8129595bcc4719427485724
Luja Manandhar <manandharluja@gmail.com>
2024-03-08 14:39:11 +0545

    carousel design and effects

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,16 +1,28 @@
-import { NgClass, NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [NgFor, NgClass],
+  imports: [NgFor, NgClass, NgIf],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
   @Input({ required: true }) images!: string[];
   currentIndex: number = 0;
+
+  nextImage() {
+    setTimeout(() => {
+      this.currentIndex = (this.currentIndex + 1)%this.images.length;
+    },1)
+  }
+
+  prevImage() {
+    setTimeout(() => {
+      this.currentIndex = this.currentIndex > 0 ? this.currentIndex - 1 : this.images.length - 1;
+    },1)
+  }
 
   imageByIndex(index: number, image: string) {
     return index;
